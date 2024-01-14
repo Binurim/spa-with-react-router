e350697a8d5e401d77d75d678a9443179a57a355
binuri <binurim95@gmail.com>
2024-01-14 15:15:25 +0530

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/products' element={<ProductsPage/>}/>
-  </Route>
-);
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage/> },
-//   { path: '/products', element: <ProductsPage/>}
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage/> },
+  { path: '/products', element: <ProductsPage/>}
+]);
 
-const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return (
