8259de63247f109a4b9c1412ac37528131b72c37
binuri <binurim95@gmail.com>
2024-01-14 16:28:34 +0530

    Add mainNavigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage/> },
-  { path: '/products', element: <ProductsPage/>}
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 
